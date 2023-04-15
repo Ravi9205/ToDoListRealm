3156556fc17c46e996ccbaa92a1c63fcd86c0073
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2023-04-15 22:22:37 +0530

    TO DO List Using Realm

---
+++ ToDoListRealm/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  ToDoListRealm
+//
+//  Created by Ravi Dwivedi on 15/04/23.
+//
+
+import Foundation
+
+
+class Contact {
+
+    var firstName:String
+    var lastName:String
+    
+    init(_firstName:String,_lastName:String) {
+        self.firstName = _firstName
+        self.lastName = _lastName
+    }
+}
--- ToDoListRealm/ViewController.swift
+++ ToDoListRealm/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var data = [Contact]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
     }
 
-
+    private func setUI(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    @IBAction func addBarButtonPressed(_ sender: Any) {
+        showAlert()
+        
+    }
 }
 
+
+extension ViewController{
+    
+    func showAlert(){
+        let alertVC = UIAlertController(title:"Add Contact", message:"Please enter your contact details", preferredStyle: .alert)
+       
+        alertVC.addTextField { firstNameField in
+            firstNameField.placeholder = "Enter your first name"
+        }
+        
+        alertVC.addTextField { lastNameField in
+            lastNameField.placeholder = "Enter your last name"
+        }
+        
+        let save = UIAlertAction(title:"Save", style: .default) { _ in
+            
+            if let firstName = alertVC.textFields?.first?.text, let lastName = alertVC.textFields?[1].text {
+                
+                print("\(firstName)  \(lastName)")
+            }
+        }
+        let cancel = UIAlertAction(title:"Cancel", style: .cancel, handler: nil)
+        
+        
+        alertVC.addAction(cancel)
+        alertVC.addAction(save)
+        self.present(alertVC, animated: true)
+        
+    }
+}
+
+//MARK:- TableView Data Source
+
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath)
+        return cell
+    }
+    
+}
+
+//MARK:- TableView Delegate
+extension ViewController:UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
